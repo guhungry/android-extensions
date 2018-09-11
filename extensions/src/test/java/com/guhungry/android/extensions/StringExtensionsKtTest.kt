396eef3aabcf5390d3c8cad49daca43e8c8ebb36
Woraphot Chokratanasombat <guhungry@gmail.com>
2018-09-11 22:48:24 +0700

    [String Extensions Test] Change: Clean up source code

--- extensions/src/test/java/com/guhungry/android/extensions/StringExtensionsKtTest.kt
+++ extensions/src/test/java/com/guhungry/android/extensions/StringExtensionsKtTest.kt
@@ -1,10 +1,6 @@
 package com.guhungry.android.extensions
 
-import android.text.Html
-import android.text.Spanned
 import androidx.core.text.HtmlCompat
-import org.hamcrest.CoreMatchers.*
-import org.hamcrest.MatcherAssert.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
